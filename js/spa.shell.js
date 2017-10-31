c1b837bd9d1539b3609f861715c9564b41273956
liujian377 <liujian12@jd.com>
2017-10-31 14:51:24 +0800

    onClickChat

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -21,19 +21,22 @@ spa.shell = (function() {
                 +'<div class="spa-shell-chat"></div>'
                 +'<div class="spa-shell-modal"></div>',
 
-            chat_extend_time: 1000,
+            chat_extend_time: 250,
             chat_retract_time: 300,
             chat_extend_height: 450,
-            chat_retract_height: 15
+            chat_retract_height: 15,
+            chat_extended_title: 'Click to retract',
+            chat_retracted_title: 'Click to extend'
         },
         // 存储整个模块中共享的动态信息
         stateMap = {
-            $container : null
+            $container : null,
+            is_chat_retracted: true
         },
         // 将jQuery集合缓存在Map中
         jqueryMap = {},
 
-        setJqueryMap, toggleChat, initModule;
+        setJqueryMap, toggleChat, onClickChat, initModule;
         // ---------- END MODULE SCOPE VARIABLES ----------
 
         // ---------- BEGIN UTILITY METHODS ---------
@@ -68,6 +71,10 @@ spa.shell = (function() {
                     {height: configMap.chat_extend_height},
                     configMap.chat_extend_time,
                     function() {
+                        jqueryMap.$chat.attr(
+                            'title', configMap.chat_extended_title
+                        );
+                        stateMap.is_chat_retracted = false;
                         if (callback) { callback(jqueryMap.$chat); }
                     }
                 );
@@ -80,6 +87,10 @@ spa.shell = (function() {
                 {height: configMap.chat_retract_height},
                 configMap.chat_retract_time,
                 function() {
+                    jqueryMap.$chat.attr(
+                        'title', configMap.chat_retracted_title
+                    );
+                    stateMap.is_chat_retracted = true;
                     if (callback) { callback(jqueryMap.$chat); }
                 }
             );
@@ -91,6 +102,10 @@ spa.shell = (function() {
         // ---------- END DOM METHODS ---------
 
         // ---------- BEGIN EVNET HANDLERS ---------
+        onClickChat = function(event) {
+            toggleChat(stateMap.is_chat_retracted);
+            return false;
+        }
         // ---------- END EVNET HANDLERS ---------
 
         // ---------- BEGIN PUBLIC METHODS ---------
@@ -101,14 +116,11 @@ spa.shell = (function() {
             $container.html(configMap.main_html);
             setJqueryMap();
 
-            // test toogle
-            setTimeout(function() {
-                toggleChat(true)
-            }, 3000);
-
-            setTimeout(function() {
-                toggleChat(false)
-            }, 8000);
+            // initialize chat slider and bind click handler
+            stateMap.is_chat_retracted = true;
+            jqueryMap.$chat
+                .attr('title', configMap.chat_retracted_title)
+                .click(onClickChat);
         }
         // End Public method /initModule/
 
